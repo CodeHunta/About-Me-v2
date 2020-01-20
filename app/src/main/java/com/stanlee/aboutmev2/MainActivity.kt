4bbb1eec78ee1705b9ca5e2d6970b419f783374c
Stanley Umezuruike <manstylo@gmail.com>
2020-01-21 00:12:50 +0100

    Actionable Intents

--- app/src/main/java/com/stanlee/aboutmev2/MainActivity.kt
+++ app/src/main/java/com/stanlee/aboutmev2/MainActivity.kt
@@ -1,14 +1,37 @@
 package com.stanlee.aboutmev2
 
+
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val aButton = findViewById<Button>(R.id.btn_phone)
+        aButton.setOnClickListener {
+            val phoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:08032324739"))
+            startActivity(phoneIntent)
+        }
+
+        val bButton = findViewById<Button>(R.id.btn_email)
+        bButton.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "manstylo@gmail.com", null))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hello Stan Lee!")
+            startActivity(emailIntent)
+        }
+
+        val cButton = findViewById<Button>(R.id.btn_location)
+        cButton.setOnClickListener {
+            val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=3 Abike Close off Miyaki street, Oworonshoki, Lagos"))
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
     }
 }
--- app/src/main/java/com/stanlee/aboutmev2/SplashScreen.kt
+++ app/src/main/java/com/stanlee/aboutmev2/SplashScreen.kt
@@ -1,12 +1,21 @@
 package com.stanlee.aboutmev2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val sButton = findViewById<Button>(R.id.btn_splash)
+        sButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
