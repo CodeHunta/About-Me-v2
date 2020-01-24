33e414c5dd33ea94ee6b3f0f3cc94f60bd640c5a
Stanley Umezuruike <manstylo@gmail.com>
2020-01-24 04:53:55 +0100

    Add Profile Picture

--- app/src/main/java/com/stanlee/aboutmev2/MainActivity.kt
+++ app/src/main/java/com/stanlee/aboutmev2/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.stanlee.aboutmev2
 
-
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,20 +14,24 @@ class MainActivity : AppCompatActivity() {
 
         val aButton = findViewById<Button>(R.id.btn_phone)
         aButton.setOnClickListener {
-            val phoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:08032324739"))
+            val phoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:+234 803 232 4739"))
             startActivity(phoneIntent)
         }
 
         val bButton = findViewById<Button>(R.id.btn_email)
         bButton.setOnClickListener {
-            val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "manstylo@gmail.com", null))
+            val emailIntent =
+                Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "manstylo@gmail.com", null))
             emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hello Stan Lee!")
             startActivity(emailIntent)
         }
 
         val cButton = findViewById<Button>(R.id.btn_location)
         cButton.setOnClickListener {
-            val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=3 Abike Close off Miyaki street, Oworonshoki, Lagos"))
+            val mapIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("geo:0,0?q=3 Abike Close off Miyaki street, Oworonshoki, Lagos")
+            )
             mapIntent.setPackage("com.google.android.apps.maps")
             startActivity(mapIntent)
         }
--- app/src/main/java/com/stanlee/aboutmev2/SplashScreen.kt
+++ app/src/main/java/com/stanlee/aboutmev2/SplashScreen.kt
@@ -18,4 +18,3 @@ class SplashScreen : AppCompatActivity() {
         }
     }
 }
-
